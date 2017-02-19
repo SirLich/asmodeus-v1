51d9552158fc48588528605802cbbe4948a2c3f4
SirLich <lrkoehler77@gmail.com>
2017-02-19 11:22:53 -0600

    Initial commit

---
+++ src/main/java/sirlich/core/AsmodeusRPG.java
@@ -0,0 +1,15 @@
+package sirlich.core;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AsmodeusRPG extends JavaPlugin{
+	// Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
